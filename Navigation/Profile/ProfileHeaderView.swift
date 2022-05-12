da4a013e0d3eda4f6d4aa9c3148f69962b5550d9
Artem Sviridov <exastronaut@yandex.ru>
2022-05-12 13:42:33 +0300

    Убрал закомментированный код

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -167,38 +167,3 @@ extension ProfileHeaderView: UITextFieldDelegate {
         endEditing(true)
     }
 }
-
-//extension ProfileHeaderView {
-//    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-//        if let viewAvailable = findPointsInsideView(parent: self, point: point, event: event) {
-//            return viewAvailable
-//        }
-//
-//        if self.point(inside: point, with: event) {
-//            return self
-//        }
-//
-//        return nil
-//    }
-//
-//    private func findPointsInsideView(parent: UIView, point: CGPoint, event: UIEvent?) -> UIView? {
-//        let allChilds = parent.subviews
-//
-//        guard allChilds.count > 0 else {
-//            return nil
-//        }
-//
-//        for chilView in allChilds.reversed() {
-//            let modifiedPoint = parent.convert(point, to: chilView)
-//            if chilView.point(inside: modifiedPoint, with: event){
-//                if let foundView = findPointsInsideView(parent: chilView, point: modifiedPoint, event: event) {
-//                    return foundView
-//                } else {
-//                    return chilView
-//                }
-//            }
-//        }
-//
-//        return nil
-//    }
-//}
